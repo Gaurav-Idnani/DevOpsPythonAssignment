75e37a702e4dca28810697ffd5493e2f2e5ccaba
Gaurav-Idnani <gidnani1401@gmail.com>
2024-06-24 21:12:55 +0530

    Password Check Solution : Q1

---
+++ solution1.py
@@ -0,0 +1,45 @@
+import re
+
+def check_password_strength(password):
+    
+    # Checks the password meets for the following criteria:
+    # - Minimum length: 8 characters.
+    # - Contains both uppercase and lowercase letters.
+    # - Contains at least one digit (0-9).
+    # - Contains at least one special character (e.g., !, @, #, $, %).
+    # return true if all criteria fulfilled otherwise false.
+    
+    if len(password) < 8:
+        return False
+    
+    if not re.search(r'[A-Z]', password):
+        return False
+    
+    if not re.search(r'[a-z]', password):
+        return False
+    
+    if not re.search(r'[0-9]', password):
+        return False
+    
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        return False
+    
+    return True
+
+def callPasswordCheck():
+    
+    # function to take user input and validate the password.
+    
+    password = input("Enter a password to check strength: ")
+    if check_password_strength(password):
+        print("Password entered is strong as it matches all criteria")
+    else:
+        print("")
+        print("Password entered is weak. It needs to fulfill the following criteria.")
+        print("-Password entered should be at least 8 characters long")
+        print("-Password entered should contains both uppercase and lowercase letters")
+        print("-Password entered should have at least one digit, and at least one special character.")
+
+callPasswordCheck()
+
+
